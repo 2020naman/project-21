c6c79d5f8a7093c967878dd80c9f0a8582c03b80
2020naman <86043450+2020naman@users.noreply.github.com>
2021-06-23 08:38:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+
+var ball;
+var ground;
+var leftSide;
+var rightSide;	
+
+
+function setup() {
+	createCanvas(1200,800);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    ground = new Ground(width/2,670,width,20);
+    leftSide=new Ground(800,600,20,120);
+	rightSide=new Ground(1100,600,20,120)
+    
+
+    var ball_options={
+      isStatic: false,
+	  restitution: 0.3,
+      friction: 0,
+	  density: 0.8
+	  
+	}
+    fill("white");
+
+    ball = Bodies.circle(200,400,15,ball_options);
+    World.add(world,ball);
+
+    rectMode(CENTER);
+    ellipseMode(RADIUS);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+
+  background(0);
+
+  ellipse(ball.position.x,ball.position.y,15);
+
+  ground.show();
+  leftSide.show();
+  rightSide.show();
+  Engine.update(engine);
+
+if(keyDown(UP_ARROW)){
+	throwBall();
+}
+ball.collide=true;
+  drawSprites();
+}
+
+function throwBall(){
+  if(keyCode === UP_ARROW){
+
+	Matter.Body.applyForce(ball,{x:0,y:0},{x:10,y:-0.8})
+  }	
+}
+
